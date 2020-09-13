723501ffd37ec7a5702779a47d3da843d71b1cca
taimoortn <taimoortn@gmail.com>
2020-09-13 14:47:19 +0500

    Push Notification working 1st phase

--- App.js
+++ App.js
@@ -1,17 +1,67 @@
-//import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { fcmService } from './src/FCMService';
+import { localNotificationService } from './src/LocalNotificationService'
 
-// create a component
 const App = () => {
+  const [messageFirebase, setMessageFirebase] = useState('')
+
+  useEffect(() => {
+    fcmService.registerAppWithFCM()
+    fcmService.register(onRegister, onNotification, onOpenNotification)
+    localNotificationService.configure(onOpenNotification)
+
+    function onRegister(token) {
+      console.log("[App] onRegister: ", token)
+    }
+
+    function onNotification(notify) {
+      console.log("[App] onNotification: ", notify)
+      const options = {
+        soundName: 'default',
+        playSound: true
+      }
+      localNotificationService.showNotification(
+        0,
+        notify.title,
+        notify.body,
+        notify, options
+      )
+    }
+
+    function onOpenNotification(notify) {
+      console.log("[App] onOpenNotification: ", notify)
+      alert('Open Notification: ', notify.body)
+      setMessageFirebase(notify.body)
+
+    }
+
+    return () => {
+      console.log('[App] unRegister')
+      fcmService.unRegister()
+      localNotificationService.unRegister()
+    }
+
+
+
+
+
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>App Taimoor</Text>
+      <Text>React Native Notification</Text>
+      <Button
+        title='Press me'
+        onPress={() => localNotificationService.cancelAllLocalNotifications()}
+      />
+      <View style={{ marginTop: 20 }}>
+        <Text>{messageFirebase}</Text>
+      </View>
     </View>
   );
 };
 
-// define your styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,6 +69,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-
-//make this component available to the app
 export default App;
